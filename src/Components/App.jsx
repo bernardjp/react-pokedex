1da7c99876aae3bd84300e635d1cee0ceb185b79
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2022-12-12 12:13:04 -0300

    changed Browser-router for a Hash-router making the app compatible with gh-pages deployment

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import styled from '@emotion/styled';
 import Header from './Header/Header';
 import { HomePage } from './BasePages/HomePage';
